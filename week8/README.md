29f934d1fabe69d670ac874f592764c5304e9934
FreeYY <yanshiye@gmail.com>
2020-11-24 02:55:39 +0000

    add week8

---
+++ week8/README.md
@@ -0,0 +1,33 @@
+## Style
+
+### Chapter 3: Arrays
+- Main data type: array – a fixed-size collection of elements.  B No explicit iteration; instead, an array is accessed by high-level, declarative operations.  - Computation unfolds as search, selection, and transformation of fixedsize data. 
+
+
+
+> Using Python+numpy, or any other **array programming language**, implement a program that takes as input an array of characters (the characters from Pride and Prejudice, for example), normalizes them, and replaces all characters with their [Leet](https://simple.wikipedia.org/wiki/Leet)  counterparts. Then it prints out the 5 most frequently occurring 2-grams. (2-grams are all 2 consecutive words in a sequence)
+
+> Note that you should stick to the array programming style as much as possible. This means: avoid explicit iteration over the elements of the array. If you find yourself writing an iteration, think of how you could do it with one or more array operations or with map/reduce operations. (Sometimes, it's not possible; but most often it is)
+
+> See starting code [here](https://github.com/crista/exercises-in-programming-style/tree/master/03-arrays).
+
+
+
+## Run
+
+```shell
+cd week8
+```
+
+```shell
+python week8.py ../pride-and-prejudice.txt 
+```
+
+Note that the result without replacing with `leet` is:
+```
+of the - 491
+to be - 445
+in the - 397
+mr darcy - 273
+to the - 268
+```
---
+++ week8/week8.py
@@ -0,0 +1,69 @@
+import sys, string
+import numpy as np
+
+"""
+Based on tf-03.py
+"""
+
+# Example input: "Hello  World!" 
+characters = np.array([' ']+list(open(sys.argv[1], encoding='UTF-8-sig').read())+[' '])
+# Result: array([' ', 'H', 'e', 'l', 'l', 'o', ' ', ' ', 
+#           'W', 'o', 'r', 'l', 'd', '!', ' '], dtype='<U1')
+
+
+# Normalize
+characters[~np.char.isalpha(characters)] = ' '
+characters = np.char.lower(characters)
+# Result: array([' ', 'h', 'e', 'l', 'l', 'o', ' ', ' ', 
+#           'w', 'o', 'r', 'l', 'd', ' ', ' '], dtype='<U1')
+
+
+### Split the words by finding the indices of spaces
+sp = np.where(characters == ' ')
+# Result: (array([ 0, 6, 7, 13, 14], dtype=int64),)
+# A little trick: let's double each index, and then take pairs
+sp2 = np.repeat(sp, 2)
+# Result: array([ 0, 0, 6, 6, 7, 7, 13, 13, 14, 14], dtype=int64)
+# Get the pairs as a 2D matrix, skip the first and the last
+w_ranges = np.reshape(sp2[1:-1], (-1, 2))
+# Result: array([[ 0,  6],
+#                [ 6,  7],
+#                [ 7, 13],
+#                [13, 14]], dtype=int64)
+# Remove the indexing to the spaces themselves
+w_ranges = w_ranges[np.where(w_ranges[:, 1] - w_ranges[:, 0] > 2)]
+# Result: array([[ 0,  6],
+#                [ 7, 13]], dtype=int64)
+
+# Voila! Words are in between spaces, given as pairs of indices
+words = list(map(lambda r: characters[r[0] + 1:r[1]], w_ranges))
+# Result: [array(['h', 'e', 'l', 'l', 'o'], dtype='<U1'), 
+#          array(['w', 'o', 'r', 'l', 'd'], dtype='<U1')]
+
+
+"""mycode"""
+leet_alpha = np.array(['4', '8', '<', '|)', '3', '|=', '[,', '#', '1', '_|', '|<', '|_', '44', '|\\|', '0', '|o', 'O_', '|2', '5', '7', '|_|', '\\/', 'VV', '%', '`/', '2'])
+words_leet = list(map(lambda w : w.astype('|S1').view(np.uint8) - 97, words))
+words_leet = list(map(lambda w : leet_alpha[w], words_leet))
+# Result: [array(['#', '3', '|_', '|_', '0'], dtype='<U3'),
+#          array(['VV', '0', '|2', '|_', '|)'], dtype='<U3')]
+"""end"""
+
+# Let's recode the characters as strings
+swords = np.array(list(map(lambda w: ''.join(w).strip(), words_leet)))
+
+"""mycode"""
+a = swords.reshape((-1, 1)).astype(object)
+# array([['#3|_|_0'], ['VV0|2|_|)']], dtype=object)
+b = np.hstack((a[:-1] + " ", a[1:]))
+# Result: array([['#3|_|_0 ', 'VV0|2|_|)']], dtype=object)
+c = np.sum(b, axis = 1).astype('str')
+# Result: array(['#3|_|_0 VV0|2|_|)'], dtype='<U17')
+"""end"""
+
+### Finally, count the word occurrences
+uniq, counts = np.unique(c, axis=0, return_counts=True)
+wf_sorted = sorted(zip(uniq, counts), key=lambda t: t[1], reverse=True)
+
+for w, c in wf_sorted[:5]:
+    print(w, '-', c)
